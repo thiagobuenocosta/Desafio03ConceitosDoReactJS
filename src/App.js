f963f9da0d08a0785fe900f475a2f749cc6a855b
Thiago Bueno Costa <th.bueno.95@gmail.com>
2020-11-26 06:22:43 -0300

    feat: repositories list, added and remove, created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { addRepositorie, listRepositories, removeRepositorie } from "./services/api"
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await addRepositorie();
+    setRepositories([ ...repositories, response.data])
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(id) { 
+    await removeRepositorie(id);
+    setRepositories(repositories.filter(repositorie => repositorie.id !== id)) 
   }
-
+  
+  useEffect(() => {
+    listRepositories().then(response => {
+      setRepositories(response.data)
+    });
+  }, [])
   return (
-    <div>
+    <div >
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo )=> (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: "http://localhost:3333",
 });
 
+export const listRepositories = () => {
+  return api.get(`/repositories`)
+}
+export const addRepositorie = () => {
+  const newRepositorie = { 
+    id: "123",
+    url: "https://github.com/josepholiveira",
+    title: "Desafio ReactJS",
+    techs: ["React", "Node.js"],
+  };
+  return api.post(`/repositories`, newRepositorie)
+}
+export const removeRepositorie = (id) => {
+  return api.delete(`/repositories/${id}`)
+}
+
 export default api;
